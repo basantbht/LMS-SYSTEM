7fa1ec01f1f5add30ffbedc685e7d269890b34dd
basantbht <bhattbasant012@gmail.com>
2025-11-18 20:15:26 +0545

    uploads

--- server/index.js
+++ server/index.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 8001;
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173' || "",
+  credentials: true              
+}));
 
 // apis
 app.use("/api/v1/media", mediaRouter)
