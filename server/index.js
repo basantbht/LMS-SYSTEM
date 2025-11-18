ff39adb8d48a2936ea9865f6cb4e643a95413296
basantbht <bhattbasant012@gmail.com>
2025-11-18 21:14:35 +0545

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 8001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173' || "",
+  origin: 'http://localhost:5173' || "https://lms-system-frontend-orcin.vercel.app",
   credentials: true              
 }));
 
